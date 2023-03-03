86935be02f491b18843ba67116737bbf130ec8d0
AntonioSotoAS <arturoantoniomontejosoto@gmail.com>
2023-03-02 23:02:27 -0500

    finished login view

--- app/src/main/java/com/antonio/music/MainActivity.kt
+++ app/src/main/java/com/antonio/music/MainActivity.kt
@@ -2,10 +2,12 @@ package com.antonio.music
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+
     }
 }
